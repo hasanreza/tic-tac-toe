e49e7c10d09a204d157ce0fce8ddf1fe84e65336
Reza Ghorbani <rezaghorbanihasan@gmail.com>
2022-12-25 17:49:44 +0330

    add bootstrap

---
+++ src/client/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './app.scss';
+
+export const App: React.FC = () => (
+  <>
+    <h1>Hello World</h1>
+  </>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './client/App';
+import { App } from './client';
+
+import './index.scss';
 
 createRoot(document.getElementById('root')!).render(<App temp={true} />);
