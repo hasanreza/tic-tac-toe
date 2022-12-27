45c6fb2ed1cfd57aa9c1ab296170ed9460fdc4f3
Reza Ghorbani <rezaghorbanihasan@gmail.com>
2022-12-27 17:35:59 +0330

    theme inspiration links

--- src/index.tsx
+++ src/index.tsx
@@ -10,3 +10,8 @@ import './styles/main.scss';
 import Game from './components/game';
 
 createRoot(document.getElementById('root')!).render(<Game />);
+
+//https://colorhunt.co/palette/eff5f5d6e4e5497174eb6440
+//https://colorhunt.co/palette/00abb33c4048b2b2b2eaeaea
+
+//https://res.cloudinary.com/dz209s6jk/image/upload/v1641566334/Challenges/xbkog35xtcpfw7ygwae9.jpg
